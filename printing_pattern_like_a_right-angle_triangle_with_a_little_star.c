3239323a04049e08cce709c085a7699d219358bb
surya prakash <119730439+9502357317@users.noreply.github.com>
2023-08-02 20:53:17 +0530

    #include <stdio.h>
    int main()
    {
        int i, j, n;
        scanf("%d", &n);
        for(i=1; i<=n; i++)
        {
            for(j=1; j<=i; j++)
            {
                printf("*");
            }
            printf("
    ");
        }
    
        return 0;
    }

--- printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
-{ 
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    { 
-        for(j=1;j<=i;j++)
-    { 
-        printf("*");
+{
+    int i, j, n;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("*");
         }
         printf("
 ");
-        }
-    
+    }
+
+    return 0;
 }
